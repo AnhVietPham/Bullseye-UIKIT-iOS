15b3794efa91c1d0650b8f114b11afff7a2793d4
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-03-11 22:14:40 +0700

    add connection Outlet

--- BullseyeUiKit/BullseyeUiKit/ViewController.swift
+++ BullseyeUiKit/BullseyeUiKit/ViewController.swift
@@ -9,20 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue : Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        startNewRound()
     }
 
     @IBAction func showAlert(){
-        let alert = UIAlertController(title: "Hello Word!", message: "This is my first app!", preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let message = "The value of the slider is now: \(currentValue)" +
+        "\nThe target value is: \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello Word!", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        startNewRound()
+    }
+
+    @IBAction func sliderMoved(_ slider: UISlider){
+        print("The value of the slider is now: \(slider.value)")
+        
+        currentValue = Int(slider.value)
     }
     
     @IBAction func knockKnock(){
@@ -33,6 +52,17 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
     }
 
 }
